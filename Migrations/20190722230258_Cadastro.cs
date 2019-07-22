8fbce6c8235a1041f6472cefa0531aac618420d7
Regiss1 <Regiss1@users.noreply.github.com>
2019-07-22 20:11:20 -0300

    Initial commit

---
+++ Migrations/20190722230258_Cadastro.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CNPJMVC.Migrations
+{
+    public partial class Cadastro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    IdLoja = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CNPJ = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.IdLoja);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/LojaCNPJ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CNPJMVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int IdLoja { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required]
+        public int CNPJ { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public int NomeCompleto { get; set; }
+    }
+}
